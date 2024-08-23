bc8923bd7f3ef732eec9bc8190b05f6d0af90156
ksimali <cassim.ali@outlook.fr>
2024-08-23 14:50:40 -0400

    Add propriété régulière au modèle TaskListComponent & Add Tasklistcomponent into app.component.htm(parent component)

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-task-list',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
-
+  tasks: Task[] = []; // une propriété régulier(incapable de passer cet info comme param pour ce composant vs propriété de type Input)
+  
 }
