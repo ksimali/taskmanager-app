86db63a89c02893e48f12ae5b3bb13a16eb328e8
ksimali <cassim.ali@outlook.fr>
2024-08-24 17:43:27 -0400

    Setup task.service component in services folder : - create class TaskService - Add task list (array) - Add @Injectable() on class TaskService - And a method updateTask()

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Task } from "../models/task.model";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TaskService {
+    tasks: Task[] =[
+        {id: "1", title: "Task 1", description: "Description for task 1", isDone: false},
+        {id: "2", title: "Task 2", description: "Description for task 2", isDone: true},
+        {id: "3", title: "Task 3", description: "Description for task 3", isDone: false},
+        {id: "4", title: "Task 4", description: "Description for task 4", isDone: true},
+    ]
+
+    updateTask(updatedTask: Task){
+        console.log(updatedTask);
+        const index = this.tasks.findIndex(task => task.id === updatedTask.id)
+        if(index !== -1){
+          this.tasks[index] = updatedTask;
+        }
+        console.log(this.tasks);
+      }
+}
